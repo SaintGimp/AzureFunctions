2172cb03e1f64927f9b220ffd79330c42c80cbe3
Eric Lee <saintgimp@hotmail.com>
2023-03-13 23:20:48 -0700

    Separate temperature watchdog and slow it down

--- CWOPGateway.cs
+++ CWOPGateway.cs
@@ -3,57 +3,26 @@ using System.Net.Sockets;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace SaintGimp
 {
-    public class CWOPGateway : FunctionBase
+    public class CWOPGateway : TemperatureFunctionBase
     {
         [FunctionName("CWOPGateway")]
         public static async Task RunAsync([TimerTrigger("45 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"Loading data from ElasticSearch...");
-            
             try
             {
-                var elasticSearchCredentials = Environment.GetEnvironmentVariable("ElasticSearchCredentials");
-            
-                var httpClient = new HttpClient();
-                var byteArray = Encoding.ASCII.GetBytes(elasticSearchCredentials);
-                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var mostRecentRecord = await GetTemperatureData(log);
 
-                var uri = $"http://elasticsearch.saintgimp.org/logstash-temperatures/_search";
-                var query = @"{
-                    ""query"": {
-                        ""match_all"": {}
-                    },
-                    ""size"": ""1"",
-                    ""sort"": [
-                        {
-                        ""@timestamp"": {
-                            ""order"": ""desc""
-                        }
-                        }
-                    ]
-                    }";
-
-                var response = await httpClient.PostAsync(uri, new StringContent(query, Encoding.ASCII, "application/json"));
-                var responseContent = await response.Content.ReadAsStringAsync();
-                log.LogInformation(responseContent);
-
-                dynamic data = JObject.Parse(responseContent);
-                var mostRecentRecord = data.hits.hits[0]._source;
                 DateTime timestamp = mostRecentRecord["@timestamp"];
                 var timeSinceLastUpdate = DateTime.UtcNow - timestamp;
-                log.LogInformation($"Timestamp is {timestamp}, difference is {timeSinceLastUpdate}");
-            
+
                 if (timeSinceLastUpdate > TimeSpan.FromMinutes(30))
                 {
                     throw new ApplicationException("No new data");
@@ -82,7 +51,6 @@ namespace SaintGimp
             catch (Exception e)
             {
                 log.LogInformation(e.ToString());
-                SendEmailNotification("Hey, I think the temperature sensors are offline!", log);
             }
         }
 
---
+++ TemperatureFunctionBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace SaintGimp
+{
+    public class TemperatureFunctionBase : FunctionBase
+    {
+        public static async Task<dynamic> GetTemperatureData(ILogger log)
+        {
+            log.LogInformation($"Loading data from ElasticSearch...");
+
+            var elasticSearchCredentials = Environment.GetEnvironmentVariable("ElasticSearchCredentials");
+
+            var httpClient = new HttpClient();
+            var byteArray = Encoding.ASCII.GetBytes(elasticSearchCredentials);
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            var uri = $"http://elasticsearch.saintgimp.org/logstash-temperatures/_search";
+            var query = @"{
+                ""query"": {
+                    ""match_all"": {}
+                },
+                ""size"": ""1"",
+                ""sort"": [
+                    {
+                    ""@timestamp"": {
+                        ""order"": ""desc""
+                    }
+                    }
+                ]
+                }";
+
+            var response = await httpClient.PostAsync(uri, new StringContent(query, Encoding.ASCII, "application/json"));
+            var responseContent = await response.Content.ReadAsStringAsync();
+            log.LogInformation(responseContent);
+
+            dynamic data = JObject.Parse(responseContent);
+            var mostRecentRecord = data.hits.hits[0]._source;
+            DateTime timestamp = mostRecentRecord["@timestamp"];
+            var timeSinceLastUpdate = DateTime.UtcNow - timestamp;
+            log.LogInformation($"Most recent timestamp is {timestamp}, {timeSinceLastUpdate} old");
+
+            return mostRecentRecord;
+        }
+    }
+}
